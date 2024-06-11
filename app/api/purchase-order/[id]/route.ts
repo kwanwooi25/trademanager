02cfbbfde462352923a57eba73d8cf4d5335ed31
Kwanwoo Jeong <kwanwoo.jeong@gmail.com>
2024-06-11 12:29:32 +0900

    delete purchase order

---
+++ app/api/purchase-order/[id]/route.ts
@@ -0,0 +1,22 @@
+import { getUserFromSession, handleFail, handlePrismaClientError, handleSuccess } from '@/lib/api';
+import { prisma } from '@/lib/prisma';
+import { HttpStatusCode } from 'axios';
+import { NextRequest } from 'next/server';
+
+export async function DELETE(req: NextRequest, { params: { id } }: { params: { id: string } }) {
+  await getUserFromSession();
+
+  if (!id) {
+    return handleFail({
+      status: HttpStatusCode.BadRequest,
+      message: 'id is required',
+    });
+  }
+
+  try {
+    await prisma.purchaseOrder.delete({ where: { id } });
+    return handleSuccess({ data: null, status: HttpStatusCode.Ok });
+  } catch (e) {
+    return handlePrismaClientError(e);
+  }
+}
--- components/pages/PurchaseOrderList/ListItem/index.tsx
+++ components/pages/PurchaseOrderList/ListItem/index.tsx
@@ -1,6 +1,18 @@
 'use client';
 
 import ProductImage from '@/components/ProductImage';
+import { createLabel } from '@/components/ProductOptionSelect/utils';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -8,10 +20,13 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { PATHS } from '@/const/paths';
+import { useToast } from '@/components/ui/use-toast';
+import { API_ROUTE, PATHS } from '@/const/paths';
 import { PURCHASE_ORDER_STATUS_LABEL_MAP } from '@/const/purchaseOrder';
+import { useAxiosError } from '@/hooks/useAxiosError';
 import { useCurrentUrl } from '@/hooks/useCurrentUrl';
 import { PurchaseOrderWithProductOption } from '@/types/purchaseOrder';
+import axios from 'axios';
 import { format } from 'date-fns';
 import { Edit2, MoreVertical, Trash2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
@@ -20,14 +35,27 @@ export default function PurchaseOrderListItem({ purchaseOrder }: Props) {
   const { id, orderedAt, status, productOption, orderedQuantity, receivedQuantity, receivedAt } =
     purchaseOrder;
 
+  const { toast } = useToast();
   const router = useRouter();
   const currentUrl = useCurrentUrl();
+  const { handleAxiosError } = useAxiosError();
 
   const handleClickEdit = () => {
     router.push(`${PATHS.EDIT_PURCHASE_ORDER}/${id}?callbackUrl=${currentUrl}`);
   };
 
-  const handleClickDelete = () => {};
+  const handleClickDelete = async () => {
+    try {
+      await axios.delete(`${API_ROUTE.PURCHASE_ORDER}/${id}`);
+      toast({
+        description: '주문 삭제 성공',
+        variant: 'success',
+      });
+      router.refresh();
+    } catch (e) {
+      handleAxiosError(e);
+    }
+  };
 
   return (
     <li className="px-4 py-2 grid items-center gap-4 grid-cols-[100px_70px_2fr_60px_1fr_100px_100px_100px__40px] border-b p-2">
@@ -41,23 +69,42 @@ export default function PurchaseOrderListItem({ purchaseOrder }: Props) {
         {receivedQuantity ? receivedQuantity.toLocaleString() : '-'}
       </span>
       <span className="text-center">{receivedAt ? format(receivedAt, 'yyyy-MM-dd') : '-'}</span>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button className="ml-auto" size="icon" variant="ghost">
-            <MoreVertical />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={handleClickEdit}>
-            <Edit2 className="mr-2 h-4 w-4" />
-            <span>수정</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem className="text-destructive" onClick={handleClickDelete}>
-            <Trash2 className="mr-2 h-4 w-4" />
-            <span>삭제</span>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+
+      <AlertDialog>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="ml-auto" size="icon" variant="ghost">
+              <MoreVertical />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={handleClickEdit}>
+              <Edit2 className="mr-2 h-4 w-4" />
+              <span>수정</span>
+            </DropdownMenuItem>
+            <AlertDialogTrigger asChild>
+              <DropdownMenuItem className="text-destructive">
+                <Trash2 className="mr-2 h-4 w-4" />
+                <span>삭제</span>
+              </DropdownMenuItem>
+            </AlertDialogTrigger>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>주문 삭제</AlertDialogTitle>
+            <AlertDialogDescription>
+              {createLabel(productOption)} {orderedQuantity.toLocaleString()}개 주문 내역을
+              삭제하시겠습니까?
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>취소</AlertDialogCancel>
+            <AlertDialogAction onClick={handleClickDelete}>삭제</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </li>
   );
 }
